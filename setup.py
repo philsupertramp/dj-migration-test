a445e9a240a1d776ffe3dd40105a2619592a03cb
Philipp Zettl <zettl.philipp@gmail.com>
2022-07-10 08:49:27 +0200

    increment version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README', 'r') as fh:
 setup(
     name='dj-migration-test',
     packages=['dj_migration_test'],
-    version=os.environ.get('TAG', 'v0.2.1'),
+    version=os.environ.get('TAG', 'v0.3.2'),
     license='MIT',
     description='Django migration test module',
     long_description=long_description,
@@ -15,7 +15,7 @@ setup(
     author='Philipp Zettl',
     author_email='philipp.zettl@godesteem.de',
     url='https://github.com/philsupertramp/dj-migration-test',
-    download_url=f'https://github.com/philsupertramp/dj-migration-test/archive/{os.environ.get("TAG", "v0.2.1")}.tar.gz',
+    download_url=f'https://github.com/philsupertramp/dj-migration-test/archive/{os.environ.get("TAG", "v0.3.2")}.tar.gz',
     keywords=['Django', 'Testing', 'Database'],
     install_requires=[
         'django>=2.2'
